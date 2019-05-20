1f45eaed8de2cb4f9a3eedf1e447a120fad919e7
Wilson <wilsonj806@gmail.com>
2019-05-20 19:42:50 -0400

    Configure Jest, edits to CI config
    - Configured Jest:
      - updated .eslintignore to ignore config files
      - slight cnage to .eslintrc
      - add the below:
        - babel.config.js
        - jest.config.js
      - add a "test" script for Jest
      - add one unit test into new file : specs/server.spec.ts
    - Update .travis.yml with environment variables and slight change to
      caching

---
+++ specs/server.spec.ts
@@ -0,0 +1,9 @@
+/**
+ * ANCHOR General test suite for the server
+ * =======================================================================
+ *
+ */
+
+test('two plus two is four', () => {
+  expect(2 + 2).toBe(4);
+});

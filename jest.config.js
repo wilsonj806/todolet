af31e9fb9382674b66bb30ef4f4f312f018c2020
Wilson <wilsonj806@gmail.com>
2019-05-23 20:24:03 -0400

    Update test coverage config

--- jest.config.js
+++ jest.config.js
@@ -36,6 +36,7 @@ module.exports = {
   // An array of regexp pattern strings used to skip coverage collection
   coveragePathIgnorePatterns: [
     "\\\\node_modules\\\\",
+    "config",
     "models",
     "specs",
     "types",

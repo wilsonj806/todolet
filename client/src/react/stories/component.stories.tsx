652a52b7186081d2e3373f409e772ec4b637b444
Wilson <wilsonj806@gmail.com>
2019-06-25 19:43:48 -0400

    Basic Storybook init
    Add additional dependencies as well

---
+++ client/src/react/stories/component.stories.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+
+storiesOf('A navbar', module)
+  .add('it renders a navbar', () => {
+    return (
+      <div>
+        Hello World!
+      </div>
+    )
+  });

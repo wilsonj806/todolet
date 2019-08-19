860c294e6e7dd655041098c699a634be37c1fe47
Wilson <wilsonj806@gmail.com>
2019-08-19 13:13:08 -0400

    Fix TextInputWrapper error
    Fix error in TextInputWrapper wherer reactHookFn would get rendered as a DOM attribute

--- client/src/containers/TextInputWrapper.tsx
+++ client/src/containers/TextInputWrapper.tsx
@@ -4,16 +4,19 @@ import { TextInputWrapperProps } from '../types/form';
 
 const TextInputWrapper: FunctionComponent<TextInputWrapperProps> = (props) => {
   const { type, classes, id, label, margin, value, reactHookFn } = props;
+  const otherProps = { ...props };
+  delete otherProps.reactHookFn;
+
   return (
     <TextField
       id={ id }
-      { ...props }
       value={ value }
       label={ label }
       margin={ margin }
       classes={ classes }
       type={ type ? type : "text" }
       onChange={ (event) => reactHookFn(event.target.value) }
+      { ...otherProps }
     />
   )
 }

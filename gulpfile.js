0db6a09ce692d103154f0fe079078a914ec1996d
Wilson <wilsonj806@gmail.com>
2019-09-17 21:47:47 -0400

    Add Gulp to clean files up after deploy
    Add Gulp and Del to project devDependencies to clean up files after deployment
    
    Add gulpfile.js with script to delete unnecessary files after deployment
    - basically deletes everything that isn't node_modules, client/build, or dist/

---
+++ gulpfile.js
@@ -0,0 +1,33 @@
+const gulp = require('gulp');
+const del = require('del');
+
+async function clean(cb) {
+  const arr = [
+    '{config,routes,specs,types,docs,__mocks__,models}/**',
+    '*.md',
+    'LICENSE',
+    '*.{js,ts,tsx}',
+    'client/**',
+    'client/.storybook',
+    '!client/build/**',
+    '!**/package-lock.json',
+    '!**/package.json',
+    '!node_modules/**',
+    '!client/node_modules/**',
+    '!Procfile',
+    '!dist/**',
+    '!gulpfile.js',
+  ];
+  const deletedPaths = await del(arr, { dryRun: false });
+
+	console.log('Files and directories that would be deleted:\n', deletedPaths.join('\n'));
+
+  cb();
+}
+
+function noTask(cb) {
+  console.log('No tasks queued up');
+  cb();
+}
+
+exports.default = gulp.series(process.env.NODE_ENV === 'production' ? clean : noTask);

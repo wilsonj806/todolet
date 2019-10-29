b72a48be29aef280296703108c7bd9299fefa41d
Wilson <wilsonj806@gmail.com>
2019-10-29 14:02:47 -0400

    Add more to logging

--- expressApp.ts
+++ expressApp.ts
@@ -75,7 +75,8 @@ app.use(passport.session());
 const staticLocation = NodeENV === 'production' ? express.static(path.join(__dirname, '../assets')) : express.static(path.join(__dirname, 'assets'))
 
 console.log(NodeENV, NodeENV === 'production','\n');
-console.log(path.join(__dirname, '../assets','\n'));
+console.log(path.join(__dirname, '../', 'assets','\n'));
+console.log('this is dirname', __dirname);
 
 app.use('/static', staticLocation);
 app.use('/dist', express.static(path.join(__dirname, 'dist')));

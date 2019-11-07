bdd5e442b79b99b83813a78e068519f692407fc9
Wilson <wilsonj806@gmail.com>
2019-11-07 15:44:23 -0500

    Partial fix for docker compose
    Add nodemon.json
    - adds a delay to restarts and adds a script for building the dev again
    
    Modify how the server connects to the database
    - it doesn't test the connection anymore
    - expressApp confirms the connection
    - note that it's going to hang if it didn't actually connect properly but still returns a readyState value of 1
    
    Add extensions for the dev:sever script to watch
    
    Update devDependencies in package.json

--- server.ts
+++ server.ts
@@ -2,31 +2,18 @@ import 'dotenv/config';
 import {
   dbName, PORT,
 } from './config/config';
-import { app, mongoose } from './expressApp';
-
-
-import Todos from './models/todo';
+import { app } from './expressApp';
 
 
 /* eslint-disable no-console */
 
-const db = mongoose.connection;
 
 /**
  * ANCHOR Check database connection
  * =============================================================
  *
  */
-db.once('open', async (): Promise<any> => {
-  try {
-    // TODO figure out how to add error checking for this Node event
-    const result: mongoose.Document | null = await Todos.findOne();
-    console.log(`Connection to MongoDB database: ${dbName} confirmed`);
-  } catch (error) {
-    console.log('Error alert, check below for additional logging \n', error);
-    throw new Error(error.message);
-  }
-});
+
 
 app.listen(PORT, (): any => { console.log(`Server started on port ${PORT}`); });
 

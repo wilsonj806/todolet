de06a2053e50004da46b482c9e97da81ff1fde2e
Wilson <wilsonj806@gmail.com>
2020-10-26 12:51:49 -0400

    Add session migration

---
+++ migrations/20201026163630-session-gen.js
@@ -0,0 +1,30 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    /**
+     * Add altering commands here.
+     *
+     * Example:
+     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
+     */
+    queryInterface.createTable('mySession', {
+      sid: Sequelize.DataTypes.STRING,
+      sess: Sequelize.DataTypes.JSONB,
+      expire: {
+        type: Sequelize.DataTypes.DATE,
+        allowNull: false
+      }
+    })
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    /**
+     * Add reverting commands here.
+     *
+     * Example:
+     * await queryInterface.dropTable('users');
+     */
+    return queryInterface.dropTable('mySession')
+  }
+};
